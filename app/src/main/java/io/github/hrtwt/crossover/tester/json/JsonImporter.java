5a1fa8dab106f91b484e63f2ff5f5a6ae2175588
hrtwt <29752757+hrtwt@users.noreply.github.com>
2022-08-29 21:19:50 +0900

    refactor test

--- app/src/main/java/io/github/hrtwt/crossover/tester/json/JsonImporter.java
+++ app/src/main/java/io/github/hrtwt/crossover/tester/json/JsonImporter.java
@@ -1,10 +1,13 @@
 package io.github.hrtwt.crossover.tester.json;
 
+import java.util.ArrayList;
+import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import io.github.hrtwt.crossover.tester.json.JsonVariant.JsonBase;
-
 
 public class JsonImporter {
 
@@ -21,5 +24,22 @@ public class JsonImporter {
 
   public static JsonVariant importVariant(final JsonElement json) {
     return setupGson().fromJson(json, JsonVariant.class);
+  }
+
+  public static JsonVariant importAdam(final String json) {
+    final JsonObject variants = new Gson().fromJson(json, JsonObject.class);
+    return JsonImporter.importVariant(variants.get("adam"));
+  }
+
+  public static List<JsonVariant> importComplementaryVariants(final String json) {
+    final JsonObject variants = new Gson().fromJson(json, JsonObject.class);
+    final JsonArray parents = variants.get("parents").getAsJsonArray();
+
+    final List<JsonVariant> ret = new ArrayList<>();
+    for (final JsonElement e : parents) {
+      final JsonVariant parent = JsonImporter.importVariant(e);
+      ret.add(parent);
+    }
+    return ret;
   }
 }
--- app/src/test/java/io/github/hrtwt/crossover/tester/json/JsonImporterTest.java
+++ app/src/test/java/io/github/hrtwt/crossover/tester/json/JsonImporterTest.java
@@ -1,24 +1,23 @@
 package io.github.hrtwt.crossover.tester.json;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import org.junit.Test;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 
 public class JsonImporterTest {
 
   @Test
-  public void testImport() throws Exception {
+  public void testImportAdam() throws Exception {
     final String json = Files.readString(Paths.get("../example/ABC139_A/variants.json"));
-    final JsonObject variants = new Gson().fromJson(json, JsonObject.class);
-    // not throws any exception
-    final JsonVariant adam = JsonImporter.importVariant(variants.get("adam"));
+    final JsonVariant adam = JsonImporter.importAdam(json);
+  }
 
-    final JsonArray parents = variants.get("parents").getAsJsonArray();
-
-    final JsonVariant parent1 = JsonImporter.importVariant(parents.get(0));
-    final JsonVariant parent2 = JsonImporter.importVariant(parents.get(1));
+  @Test
+  public void testImportComplementaryVariants() throws Exception {
+    final String json = Files.readString(Paths.get("../example/ABC139_A/variants.json"));
+    final List<JsonVariant> parents = JsonImporter.importComplementaryVariants(json);
+    assertThat(parents).hasSize(2);
   }
 }
