1840fade5c7fc0d49385104a0875ae845278c58b
hrtwt <29752757+hrtwt@users.noreply.github.com>
2022-08-31 13:59:44 +0900

    impl VariantBuilder

---
+++ app/src/main/java/io/github/hrtwt/crossover/tester/VariantBuilder.java
@@ -0,0 +1,110 @@
+package io.github.hrtwt.crossover.tester;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import io.github.hrtwt.crossover.tester.json.JsonVariant;
+import io.github.hrtwt.crossover.tester.json.JsonVariant.JsonBase;
+import jp.kusumotolab.kgenprog.ga.variant.Base;
+import jp.kusumotolab.kgenprog.ga.variant.EmptyHistoricalElement;
+import jp.kusumotolab.kgenprog.ga.variant.Gene;
+import jp.kusumotolab.kgenprog.ga.variant.Variant;
+import jp.kusumotolab.kgenprog.ga.variant.VariantStore;
+import jp.kusumotolab.kgenprog.project.ASTLocation;
+import jp.kusumotolab.kgenprog.project.ASTLocations;
+import jp.kusumotolab.kgenprog.project.LineNumberRange;
+import jp.kusumotolab.kgenprog.project.Operation;
+import jp.kusumotolab.kgenprog.project.TestFullyQualifiedName;
+import jp.kusumotolab.kgenprog.project.test.TestResults;
+
+public class VariantBuilder {
+
+  public static Variant makeVariant(final JsonVariant json, VariantStore store) {
+    Variant parent = store.getInitialVariant();
+    final List<Base> bases = new ArrayList<>();
+    for (final JsonBase jb : json.bases) {
+      final Base b = makeBase(jb, parent);
+      bases.add(b);
+      parent = store.createVariant(new Gene(bases), EmptyHistoricalElement.instance);
+    }
+
+    if (!hasSameTestResults(parent, json)) {
+      throw new IllegalStateException("failed to build variant");
+    }
+
+    return parent;
+  }
+
+  public static boolean hasSameTestResults(final Variant variant, final JsonVariant json) {
+    final TestResults one = variant.getTestResults();
+    final Map<TestFullyQualifiedName, Boolean> other = json.testResults;
+
+    if (one.getExecutedTestFQNs().size() != other.size()) {
+      return false;
+    }
+
+    for (final TestFullyQualifiedName fqn : other.keySet()) {
+      if (one.getTestResult(fqn).failed == other.get(fqn)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static Base makeBase(final JsonBase jsonBase, final Variant parent) {
+    final Statement stmt = parseSnippet(jsonBase.snippet, jsonBase.snippetNodeType);
+    final Operation operation = jsonBase.operationType.constructOperation(stmt);
+
+    final ASTLocation location =
+        findLocationByLineNo(parent, jsonBase.startLineNo, jsonBase.endLineNo);
+
+    return new Base(location, operation);
+  }
+
+  public static Statement parseSnippet(final String snippet, final int nodeType) {
+    // jdt ast parser can't parse code snippet that consists only statement
+    // so adding class and method definition is needed.
+    final String javaCode = "class Temp{void temp(){" + snippet + "}}";
+
+    final ASTParser parser = ASTParser.newParser(AST.JLS8);
+    parser.setSource(javaCode.toCharArray());
+    final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+    final TypeDeclaration type = (TypeDeclaration) cu.types().get(0);
+    final MethodDeclaration method = type.getMethods()[0]; // the first method
+    final Block block = method.getBody();
+    final List<?> stmts = block.statements();
+
+    final Statement theStatement = (Statement) stmts.get(0);
+    if (theStatement.getNodeType() != nodeType) {
+      throw new IllegalStateException("failed to parse snippet");
+    }
+
+    return theStatement;
+  }
+
+  public static ASTLocation findLocationByLineNo(
+      final Variant v, final int startLineNo, final int endLineNo) {
+    final Predicate<LineNumberRange> isSameRange =
+        e -> e.start == startLineNo && e.end == endLineNo;
+    final ASTLocations locs = v.getGeneratedSourceCode().getProductAsts().get(0).createLocations();
+    final List<ASTLocation> candidates =
+        IntStream.rangeClosed(startLineNo, endLineNo)
+            .mapToObj(locs::infer)
+            .flatMap(Collection::stream)
+            .distinct()
+            .filter(e -> isSameRange.test(e.inferLineNumbers()))
+            .collect(Collectors.toList());
+    return candidates.get(0); // todo
+  }
+}
---
+++ app/src/test/java/io/github/hrtwt/crossover/tester/VariantBuilderTest.java
@@ -0,0 +1,43 @@
+package io.github.hrtwt.crossover.tester;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import io.github.hrtwt.crossover.tester.json.JsonVariant;
+import io.github.hrtwt.crossover.tester.json.JsonVariantParser;
+import jp.kusumotolab.kgenprog.ga.variant.Variant;
+import jp.kusumotolab.kgenprog.ga.variant.VariantStore;
+
+public class VariantBuilderTest {
+
+  private VariantStore store;
+
+  @Before
+  public void setupVariantStore() {
+    store = Util.createVariantStore(Paths.get("../example/ABC139_A/"));
+  }
+
+  public List<JsonVariant> parsedVariants() throws IOException {
+    final String json = Files.readString(Paths.get("../example/ABC139_A/variants.json"));
+    return JsonVariantParser.parseComplementaryVariants(json);
+  }
+
+  @Test
+  public void testMakeVariant01() throws Exception {
+    final JsonVariant jsonVariant = parsedVariants().get(0);
+    final Variant v = VariantBuilder.makeVariant(jsonVariant, store);
+    assertThat(v).isNotNull(); // todo strict check
+  }
+
+  @Test
+  public void testHasSameTestResults01() throws Exception {
+    final List<JsonVariant> jsonVariants = parsedVariants();
+    final Variant v0 = VariantBuilder.makeVariant(jsonVariants.get(0), store);
+    assertThat(VariantBuilder.hasSameTestResults(v0, jsonVariants.get(0))).isTrue();
+    assertThat(VariantBuilder.hasSameTestResults(v0, jsonVariants.get(1))).isFalse();
+  }
+}
